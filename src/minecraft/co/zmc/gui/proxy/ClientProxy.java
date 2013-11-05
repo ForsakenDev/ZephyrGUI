67ff953116f905624ed0d9f3b4ce766b6f486152
WinMac32 <seth@viaware.ca>
2013-11-04 20:07:46 -0700

    Add mcmod.info

--- src/minecraft/co/zmc/gui/proxy/ClientProxy.java
+++ src/minecraft/co/zmc/gui/proxy/ClientProxy.java
@@ -3,8 +3,6 @@ package co.zmc.gui.proxy;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import co.zmc.gui.rendering.RenderEvent;
-import co.zmc.gui.rendering.Renderer;
-import co.zmc.gui.renderobjects.RectangleObject;
 
 public class ClientProxy extends Proxy {
 
